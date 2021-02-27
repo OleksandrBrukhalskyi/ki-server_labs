b9bb7ee4de72ac3671162f9bbbd1555eebc25b57
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-02-27 17:29:43 +0200

    delete sql file

---
+++ lab4/owners_insert.sql
@@ -0,0 +1,5 @@
+COPY owners 
+FROM 'D:/KI-Server-labs/lab4/csv/owners.csv'
+DELIMITER ',' CSV HEADER;
+
+select * from owners
---
+++ lab4/specialties_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO specialties (specialty) VALUES 
+('Хірург')
+
+INSERT INTO specialties (specialty) VALUES
+('Анастезіолог'),
+('Медсестра'),
+('Терапевт'),
+('Бактеріолог')
