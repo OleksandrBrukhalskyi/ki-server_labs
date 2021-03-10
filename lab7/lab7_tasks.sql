ea4ae878920c0102910ffa6156883e8b2754febf
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-03-10 11:01:48 +0200

    additional queries

--- lab7/lab7_tasks.sql
+++ lab7/lab7_tasks.sql
@@ -28,6 +28,7 @@ from owners as o
 join pets as p ON o.id = p.owner_id
 join visits as v ON p.id = v.pet_id
 group by v.goal_of_visit 
+order by COUNT(*) desc
 
 
 SELECT COUNT(p.name) as "К-сть тварин", o.home_address as "Ім'я", o.firstname as "Прізвище"
@@ -43,8 +44,36 @@ where not exists (select * from visits v where v.id = p.id)
 
 
 
+-- Top user by qty of pets
+SELECT  COUNT(p.name) as "К-сть тварин", o.home_address as "Ім'я", o.firstname as "Прізвище"
+from owners as o
+join pets as p ON o.id = p.owner_id
+group by o.home_address, o.firstname
+order by COUNT(p.name) desc limit 1
 
 
+
+--pet's visit breed starts with 'Л'
+SELECT o.id, o.home_address as "Ім'я", o.firstname as "Прізвище", p.breed as "Порода", p.name as "Кличка",v.goal_of_visit as "Мета візиту" ,v.visit_date as "Дата візиту"
+from owners as o
+join pets as p ON o.id = p.owner_id
+join visits as v ON p.id = v.pet_id
+where p.breed like 'Л%'
+
+
+-- owners with pets ordered by breed
+SELECT o.id, o.home_address as "Ім'я", o.firstname as "Прізвище", p.breed as "Порода", p.name as "Кличка"
+from owners as o
+join pets as p
+ON o.id = p.owner_id
+order by p.breed
+
+
+-- qty of pets's names ordered by qtys in desc
+SELECT  COUNT(*) as "К-сть", p.name as "Кличка"
+from pets as p
+group by p.name
+order by COUNT(*) desc
 
 
 
